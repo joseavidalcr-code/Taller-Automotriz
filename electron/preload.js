24a1026cc4008e9c09f8a9250ccd5ce7aa4ea7fa
joseavidalcr-code <joseavidalcr@gmail.com>
2026-08-31 20:22:48 +0200

    Completar puente seguro para gestión de mecánicos

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,11 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('tallerDesktop', {
-  openMechanicPanel: () => ipcRenderer.invoke('open-mechanic-panel')
+  openMechanicPanel: () => ipcRenderer.invoke('open-mechanic-panel'),
+  listMechanics: () => ipcRenderer.invoke('list-mechanics'),
+  loginMechanic: (id, pin) => ipcRenderer.invoke('login-mechanic', { id, pin }),
+  setMechanicPin: (id, pin, adminPassword) => ipcRenderer.invoke('set-mechanic-pin', { id, pin, adminPassword }),
+  setMechanicEnabled: (id, enabled, adminPassword) => ipcRenderer.invoke('set-mechanic-enabled', { id, enabled, adminPassword })
 });
 
 function addMechanicButton() {
