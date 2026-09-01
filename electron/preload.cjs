c9ff9490884252a1bb08f9cebc2b964cfb62324c
joseavidalcr-code <joseavidalcr@gmail.com>
2026-09-01 21:15:45 +0200

    fix: usar preload CommonJS compatible con Electron

---
+++ electron/preload.cjs
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('tallerDesktop', {
+  openMechanicPanel: () => ipcRenderer.invoke('open-mechanic-panel'),
+  listMechanics: () => ipcRenderer.invoke('list-mechanics'),
+  loginMechanic: (id, pin) => ipcRenderer.invoke('login-mechanic', { id, pin }),
+  setMechanicPin: (id, pin, adminPassword) => ipcRenderer.invoke('set-mechanic-pin', { id, pin, adminPassword }),
+  setMechanicEnabled: (id, enabled, adminPassword) => ipcRenderer.invoke('set-mechanic-enabled', { id, enabled, adminPassword })
+});
